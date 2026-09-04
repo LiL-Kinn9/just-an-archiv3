1a241a164546267b5f60bd830e871548bf6ce0cd
kiet <kietduong2982005@gmail..com>
2026-09-04 17:39:16 -0500

    update story fix

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -19,12 +19,12 @@ function About({ isAboutOpen, hasAboutStarted, uiTheme }) {
           <h2>ABOUT PROJECT</h2>
 
           <p>Dự án này được xây dựng với mục đích học hỏi, lưu trữ kỉ niệm.</p>
-          <br />
+
           <p>
             Những nội dung bên trong dự án không nhằm mục đích cổ súy, công kích
             hay báng bổ bất cứ tổ chức hay cá nhân nào.
           </p>
-          <br />
+
           <p>
             Cũng như là sự tri ân lớn với những người rời đi hay ở lại, kể cả xa
             lạ, góp phần hoàn thành dự án này.
@@ -40,14 +40,14 @@ function About({ isAboutOpen, hasAboutStarted, uiTheme }) {
             Just An Archiv3 là một kho lưu trữ mang giá trị nghệ thuật dưới góc
             nhìn cá nhân mình.
           </p>
-          <br />
+
           <p>Ý niệm về tình yêu, về sự trưởng thành và về những nỗi đau.</p>
-          <br />
+
           <p>
             Những câu chuyện được ẩn ý tường thuật lại dựa trên những mảnh ghép
             trong quá trình trưởng thành của mình.
           </p>
-          <br />
+
           <p>
             Mọi cái tên, mọi diễn biến là một câu chuyện thật sự tồn tại dưới
             lăng kính của nhân vật hư cấu.
@@ -60,14 +60,14 @@ function About({ isAboutOpen, hasAboutStarted, uiTheme }) {
           <h2>DEVELOPMENT</h2>
 
           <p>Directed & Designed by ___Kninn9.</p>
-          <br />
+
           <p>
             ARTWORKS & MUSIC. ALL RIGHTS RESERVED: MCK, FujiByNight, Phuong Vu,
             Trung Bao, Fustic, Maiki, N0L4BL3, AAA Music Studio.
           </p>
-          <br />
+
           <p>Thank To: HieuImBa, BeyBipuii, Thanh Nhi, Tuan Kien (AAA).</p>
-          <br />
+
           <p>Special Thank To: T. Kristen, who contributed the most.</p>
         </div>
       </div>
